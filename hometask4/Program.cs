3f33ce2aa9e72d2b887bc4cfc8c8f8b438e1ee05
axolotl87 <aka071087@gmail.com>
2022-09-19 11:15:08 +0200

    add task4

---
+++ hometask4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+using static System.Console;
+
+Clear();
+Write("Введеите число: ");
+int num=Convert.ToInt32(ReadLine());
+
+int count = 1;
+while(count < num)
+{
+    if (count%2 == 0)
+    {
+        WriteLine($"{count}");
+    }
+    count++;
+}
