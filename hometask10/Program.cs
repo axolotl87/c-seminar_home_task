dcd55d0d60f5554c4d38eecf75bfd4e2ea216289
axolotl87 <aka071087@gmail.com>
2022-09-24 19:22:21 +0200

    add hometask10

---
+++ hometask10/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+using static System.Console;
+Clear();
+WriteLine("Введите 3хзначное число: ");
+string num = ReadLine()!;
+if (num.Length == 3)
+{
+    int number = Convert.ToInt32(num);
+    int result = 0;
+    while(number > 10)
+    {
+        result=number%10;
+        number/=10;
+    }
+    WriteLine(result);
+
+}
+else
+{
+    WriteLine($"вы ввели не 3хзначное число");
+}
