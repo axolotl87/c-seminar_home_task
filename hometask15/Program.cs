6e109da596e2141e2d510b054a80887e1cd35d92
axolotl87 <aka071087@gmail.com>
2022-09-24 20:20:51 +0200

    add hometask15

---
+++ hometask15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+using static System.Console;
+
+Clear();
+Write("Введеите порядковый номер дня недели: ");
+int number = Convert.ToInt32(ReadLine());
+if(number > 7 || number < 1)
+{
+    WriteLine("Нет такого дня недели((");
+}
+else
+{
+    if(number < 6)
+    {
+        WriteLine("Нет");
+    }
+    else
+    {
+        WriteLine("Да");
+    }
+}
