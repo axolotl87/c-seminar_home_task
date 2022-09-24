ab2eec9eced8dac530b0241654eb394c9b77f97d
axolotl87 <aka071087@gmail.com>
2022-09-24 20:04:28 +0200

    add hometask13

---
+++ hometask13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+
+using static System.Console;
+Clear();
+WriteLine("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+int result = 0;
+int count = 0;
+while(number > 0 && count < 3)
+{
+    result=number%10;
+    number/=10;
+    count ++;
+}
+if(count>2)
+{
+    WriteLine(result);
+}
+else
+{
+    WriteLine("третьей цифры в числе нет");
+}
+
