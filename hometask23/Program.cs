c07f6146c77746bb4c5189708854e1d45e22000e
axolotl87 <aka071087@gmail.com>
2022-09-26 12:06:40 +0200

    add hometask23

---
+++ hometask23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+int number = int.Parse(ReadLine()!);
+if(number<1) WriteLine("вы ввели некорректное число");
+WriteLine();
+for (int i = 1; i <= number; i++)
+{
+    double cube = Math.Pow(i, 3);
+    Write($"{cube}, ");
+}
